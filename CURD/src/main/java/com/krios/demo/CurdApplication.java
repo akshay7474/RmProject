600ba576a5252e7c4be691b28abd082b56ef9a1a
akshay7474 <s.akshayingole@gmail.com>
2019-03-10 20:51:10 +0530

    Update CurdApplication.java

--- CURD/src/main/java/com/krios/demo/CurdApplication.java
+++ CURD/src/main/java/com/krios/demo/CurdApplication.java
@@ -3,7 +3,7 @@ package com.krios.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
+//////////////////
 @SpringBootApplication
 @EntityScan(basePackages="com.krios.demo")
 public class CurdApplication {
